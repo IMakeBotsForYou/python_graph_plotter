bf05360f78355582b85fe7e3754ebdbf21390e90
IMakeBotsForYou <40301997+IMakeBotsForYou@users.noreply.github.com>
2020-11-14 11:09:50 +0200

    refrag

--- grapher.py
+++ grapher.py
@@ -19,8 +19,8 @@ def get_fx():
     print("Available functions: cos, sin, tan, sqrt, pow")
     print("If you use trigo functions, recommended scaling: off")
     amount = int(input("How many graphs do you want?  "))
-    for g in range(amount):
-        graphs.append(input(f'Input #{g}. f(x) = '))
+    for gr_n in range(amount):
+        graphs.append(input(f'Input #{gr_n}. f(x) = '))
         custom_range = input("Custom range? y/n  ")
         if custom_range == 'y':
             top_y.append(int(input("Input top of range = ")))
@@ -42,7 +42,7 @@ rel_unit = 1
 # IDEA: Idea for relative unit concept given by Ron Fefer .
 ###
 
-# All occurances of X in the function
+# All occurrences of X in the function
 temp = ""
 func = ""
 
@@ -92,9 +92,10 @@ def fix_function(fx):
         elif (fx[index].isalpha() or fx[index] == "(") and fx[index - 1].isnumeric():
             fx = insert(fx, index, '*')
             index += 1
-        elif (fx[index].isnumeric() and fx[index-1]==")"):
+        elif fx[index].isnumeric() and fx[index - 1] == ")":
             fx = insert(fx, index, '*')
             index += 1
+            '''
     # 5cos(x) => 5 * cos(1 * x)
     # if (len(xlist) > 0):
     #     if xlist[0] == 0:
@@ -106,18 +107,19 @@ def fix_function(fx):
     #     i = xlist[-1]
     #     temp = standAloneX(i)
     #     del xlist[-1]
-
+'''
     return fx.replace("x", "(x)")
 
 
 # Initialize axis
-###fix_function(func)
+# fix_function(func)
 xvalues = list(range(-xSize, xSize + 1))
-
+'''
 # xvalues = np.arange(-xSize, xSize+1, 1.0)
 # yvalues = [eval(func.replace("x", str(x))) for x in xvalues]
 
-###print(f'Plotting: f(x) = {save}')
+###print(f'Plotting: f(x) = {save}')'
+'''
 # Calculate y values, and for x that fall out of arange
 # set value to 0, to not change scale of graph.
 yvalues = []
@@ -133,7 +135,7 @@ def calc_function(fx):
             yvalues.append(0)
 
 
-###calc_function(func)
+# calc_function(func)
 need_unit = False
 
 
@@ -155,9 +157,9 @@ def linear():
             need_unit = True
 
 
-###linear()
+# linear()
 # If not, calculate scale.
-def needUnit():
+def need_unit_check():
     global need_unit, xSize, yvalues, left_x, right_x, ySize, current_graph, rel_unit
 
     if need_unit:
@@ -171,7 +173,7 @@ def needUnit():
     print("Scale: " + str(rel_unit) + ":1")
 
 
-###needUnit()
+# needUnit()
 # Print the scale
 
 # Translate y values to match scale
@@ -181,7 +183,7 @@ def fix_y_values():
     yvalues = [round(y / rel_unit) for y in yvalues]
 
 
-###fix_y_values()
+# fix_y_values()
 graph = ""  # Initiate empty string for graph
 
 
@@ -201,16 +203,16 @@ def draw_axis():
 
 # Bring coordinate system to 0, 0 of axis
 # instead of top left corner
-def centaralize(x, y):
+def centralize(x, y):
     x = x + xSize
     y = -y + ySize
-    return (x, y)
+    return x, y
 
 
 # Translate (x,y) pair of coordinates
 # into an index in the print string
-def Translate(x, y):
-    (x, y) = centaralize(x, y)
+def translate(x, y):
+    (x, y) = centralize(x, y)
     return (xSize * 2 + 2) * y + x
 
 
@@ -226,23 +228,23 @@ def fill_graph(ch):
     for y in range(top_y[current_graph], bottom_y[current_graph] - 1, -1):  # Top->bottom
         for x in range(left_x[current_graph], right_x[current_graph] + 1):  # Left->Right
             if yvalues[x + xSize] == y:  # If yvalues has current value at current x
-                graph = draw(Translate(x, y), ch)  # Draw the point
+                graph = draw(translate(x, y), ch)  # Draw the point
 
 
 # Show how big the graph really is
 def add_graph_numbers():
     global graph
-    graph = draw(Translate(-2, 0), '(0,0)')
-    graph = draw(Translate(-len(f'({round(ySize * rel_unit)},0)') // 2 + 1, ySize), f'(0,{round(ySize * rel_unit)})')
-    graph = draw(Translate(-len(f'({round(ySize * rel_unit)},0)') // 2 + 1, -ySize), f'(0,{round(-ySize * rel_unit)})')
-    graph = draw(Translate(xSize - len(f'(0,{round(xSize  * rel_unit)})') + 1, 0), f'(0,{round(xSize * rel_unit)})')
-    graph = draw(Translate(-xSize, 0), f'(0,{round(-xSize * rel_unit)})')
+    graph = draw(translate(-2, 0), '(0,0)')
+    graph = draw(translate(-len(f'({round(ySize * rel_unit)},0)') // 2 + 1, ySize), f'(0,{round(ySize * rel_unit)})')
+    graph = draw(translate(-len(f'({round(ySize * rel_unit)},0)') // 2 + 1, -ySize), f'(0,{round(-ySize * rel_unit)})')
+    graph = draw(translate(xSize - len(f'(0,{round(xSize * rel_unit)})') + 1, 0), f'(0,{round(xSize * rel_unit)})')
+    graph = draw(translate(-xSize, 0), f'(0,{round(-xSize * rel_unit)})')
 
 
 draw_axis()
 
 
-def makefull_function(fx, ch):
+def make_full_function(fx, ch):
     global yvalues
     fix_function(fx)
     make_temp(fx)
@@ -250,9 +252,9 @@ def makefull_function(fx, ch):
     func = fix_function(fx)
     print("Plotting: " + func)
     calc_function(func)
-    if (unit_list[current_graph] == 'y'):
+    if unit_list[current_graph] == 'y':
         linear()
-        needUnit()
+        need_unit_check()
     fill_graph(ch)
     # add_graph_numbers()
 
@@ -263,7 +265,7 @@ def makefull_function(fx, ch):
 # add_graph_numbers()
 get_fx()
 for g in range(len(graphs)):
-    makefull_function(graphs[g], ch_list[g])
+    make_full_function(graphs[g], ch_list[g])
 add_graph_numbers()
 # Draw another functions
 
